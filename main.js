bf8b134a87b892c50124404c3e8f0b2c845aabbc
Wojtek Warmuz <wwarmuz@gmail.com>
2020-07-22 12:05:10 +0200

    add header css

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-const runner = document.querySelector('.you');
+const initButton = document.querySelector(".init");
+const header = document.querySelector("header");
+
+const runner = document.querySelector(".you");
 let position = 0;
 
 const raceFunction = (e) => {
-    if (e.keyCode === 32) {
-        console.log('działa');
-        runner.style.left = `${position}%`;
-        position++;
-    }
+  if (e.keyCode === 32) {
+    console.log("działa");
+    runner.style.left = `${position}%`;
+    position++;
+  }
+};
+const initFunction = () => {
+  header.classList.toggle("activeHeader");
+};
 
-}
-
-window.addEventListener('keyup', raceFunction);
+initButton.addEventListener("click", initFunction);
+window.addEventListener("keyup", raceFunction);
