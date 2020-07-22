99b23d3e095b59d890cbfc60cf940591e1103e12
Wojtek Warmuz <wwarmuz@gmail.com>
2020-07-22 22:11:18 +0200

    start counting project

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const initButton = document.querySelector(".init");
 const header = document.querySelector("header");
 const tittle = document.querySelector("header p");
 const gameMode = document.querySelector(".gameMode");
-const timeTrialBtn = document.querySelector("timeTrial");
+const timeTrialBtn = document.querySelector(".timeTrial");
 const chalengeBtn = document.querySelector(".chalenge");
-
+const divCounter = document.querySelector(".counter");
+let countNumber = 3;
 const runner = document.querySelector(".you");
 let position = 0;
 
@@ -23,6 +24,24 @@ const initFunction = () => {
   gameMode.classList.add("active");
 };
 
-initButton.addEventListener("click", initFunction);
+const countFunction = () => {
+  if (countNumber > 0) {
+    divCounter.textContent = countNumber;
+    countNumber--;
+  } else {
+    clearInterval(myInterval);
+  }
+};
 
+const timeTrialFunction = () => {
+  gameMode.classList.remove("active");
+  gameMode.classList.add("gameMode");
+  const myInterval = setInterval(countFunction, 1000);
+  //       if (divCounter.textContent === 1) {
+  //         clearInterval(myInterval);
+  //   };
+};
+
+initButton.addEventListener("click", initFunction);
+timeTrialBtn.addEventListener("click", timeTrialFunction);
 window.addEventListener("keyup", raceFunction);
