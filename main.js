ca5d6bd01c556322a5b218b7205b91bbfde07034
Wojtek Warmuz <wwarmuz@gmail.com>
2020-07-22 18:51:25 +0200

    add gameMode section

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const initButton = document.querySelector(".init");
 const header = document.querySelector("header");
+const tittle = document.querySelector("header p");
+const gameMode = document.querySelector(".gameMode");
+const timeTrialBtn = document.querySelector("timeTrial");
+const chalengeBtn = document.querySelector(".chalenge");
 
 const runner = document.querySelector(".you");
 let position = 0;
@@ -13,7 +17,12 @@ const raceFunction = (e) => {
 };
 const initFunction = () => {
   header.classList.toggle("activeHeader");
+  tittle.classList.add("activeHeaderP");
+  initButton.classList.add("hide");
+  gameMode.classList.remove("gameMode");
+  gameMode.classList.add("active");
 };
 
 initButton.addEventListener("click", initFunction);
+
 window.addEventListener("keyup", raceFunction);
